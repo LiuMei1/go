4042afa5765200fb92203b7ea2509f0bc90a603c
LiuMei <1812446981@qq.com>
2017-04-01 10:10:39 +0800

    五子棋

--- go/src/com/niuniu/gq/App.java
+++ go/src/com/niuniu/gq/App.java
@@ -2,4 +2,7 @@ package com.niuniu.gq;
 
 public class App {
 
+	public static void main(String[] args) {
+		AppFrame af = new AppFrame();
+	}
 }
---
+++ go/src/com/niuniu/gq/AppFrame.java
@@ -0,0 +1,22 @@
+package com.niuniu.gq;
+
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6652016715219382972L;
+
+	public AppFrame() {
+		
+		setTitle("五子棋");
+		setSize(500,500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+}
